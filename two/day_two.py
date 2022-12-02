1a49fe00a9564b05581fe89a9a24da63c32af6e1
RM35 <92856544+RM35@users.noreply.github.com>
2022-12-02 05:45:16 +0000

    day two

---
+++ two/day_two.py
@@ -0,0 +1,58 @@
+
+def one():
+    with open("one/day_two_input.txt", "r", encoding="utf-8") as f:
+        count = 0
+        cals = []
+        for line in f.readlines():
+            A = line.split(" ")[0]
+            B = line.split(" ")[1].rstrip()
+            if A == "A" and B == "X":
+                count += 3
+                count += 1
+            if A == "B" and B == "Y":
+                count += 3
+                count += 2
+            if A == "C" and B == "Z":
+                count += 3
+                count += 3
+            if A == "A" and B == "Y":
+                count += 6
+                count += 2
+            if A == "C" and B == "Y":
+                count += 2
+            if A == "B" and B == "Z":
+                count += 6
+                count += 3
+            if A == "B" and B == "X":
+                count += 1
+            if A == "A" and B == "Z":
+                count += 3
+            if A == "C" and B == "X":
+                count += 6
+                count += 1
+        print(count)
+
+def two():
+    with open("one/day_two_input.txt", "r", encoding="utf-8") as f:
+        count = 0
+        cals = []
+        for line in f.readlines():
+            A = line.split(" ")[0]
+            B = line.split(" ")[1].rstrip()
+            if B == "X": 
+                count += 0
+                if A == "A": count += 3
+                if A == "B": count += 1
+                if A == "C": count += 2
+            if B == "Y": 
+                count += 3
+                if A == "A": count += 1
+                if A == "B": count += 2
+                if A == "C": count += 3
+            if B == "Z": 
+                count += 6
+                if A == "A": count += 2
+                if A == "B": count += 3
+                if A == "C": count += 1
+            pass
+        print(count)
