4860e21857ccabad0979a62f4249e754a19a10a4
RM35 <92856544+RM35@users.noreply.github.com>
2022-12-05 06:28:14 +0000

    doc: add comment day five

--- five/day_five.py
+++ five/day_five.py
@@ -1,5 +1,16 @@
 ## try everything until it works. 
 
+## Post solution summary:
+# 1: list slice to get each row on the initial stacks.
+# 2: check for ascii 48 to 58 to detect the end of the initial stacks
+# 3: filter() to remove empty strings and take every other crate stack to
+# tidy up unused data.
+# 4: Find move commands by line starting "move". split and get the values
+# 5: use pop/insert to move around the crates
+# 6: for part 2 collect all the pops in each move and use + operator to
+# basically do a list.extend() but to the front of each stack
+# 7: crate stacks are in oreder top to bottom so take [0] for each top crate.
+
 def one():
     ans = ""
     crates = []
