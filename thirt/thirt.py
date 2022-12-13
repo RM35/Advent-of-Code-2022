fcf342e164095b47db357c721e4114aebea3d489
RM35 <92856544+RM35@users.noreply.github.com>
2022-12-13 08:00:49 +0000

    fix: refactor the compare func to deal with the list == list cases where we do nothing

--- thirt/thirt.py
+++ thirt/thirt.py
@@ -1,77 +1,41 @@
 from collections import deque
 
 def compare(l1, l2):
-    equal = True
-    if equal == False: return False
+    # Can be int int, int list or list list
 
+    # Deal with list int/ int list
     if type(l1) is int and type(l2) is list: l1 = [l1]
     if type(l2) is int and type(l1) is list: l2 = [l2]
 
-    print(f'Comparing {l1} and {l2}')
-
+    # deal with int int. l1 less than or equal is correct
+    # Need to distinguish if equal/below or above
     if type(l1) is int and type(l2) is int:
-        print(f'Comparison: {l1 <= l2}')
-        return l1 <= l2
-
+        if l1 == l2: return 0
+        if l1 < l2: return -1
+        if l1 > l2: return 1
+    
+    # deal with empty lists
     if type(l1) is list and type(l2) is list:
-        if len(l1) == 0 and len(l2) > 0:
-            print(f'Comparison: True')
-            return True
-            
-        if len(l1) > 0 and len(l2) == 0: 
-            print(f'Comparison: False')
-            equal = False
-            return False
-
+        if len(l1) == 0 and len(l2) > 0: return -1
+        if len(l1) > 0 and len(l2) == 0: return 1
+        if len(l1) == 0 and len(l2) == 0: return 0
+    
+    # deal with list list
     if type(l1) is list and type(l2) is list:
-
-        # final_depth = True
-        # for signal in l1:
-        #     if type(signal) is list: 
-        #         final_depth = False
-        # for signal in l2:
-        #     if type(signal) is list: 
-        #         final_depth = False
-        
-        # if final_depth:
-        #     min_len = min(len(l1), len(l2))
-        #     for i in range(min_len):
-        #         print(f'Comparing {l1[i]} and {l2[i]}')
-        #         if l1[i] > l2[i]: 
-        #             print(f'Comparison: False')
-        #             equal = False
-        #             return False
-        #         if i == (min_len-1) and len(l1) > len(l2) and l1[i] == l2[i]:
-        #             print(f'Comparison: False')
-        #             equal = False
-        #             return False
-        #         if i == (min_len-1) and len(l1) == len(l2) and l1[i] == l2[i]:
-        #             print(f'Comparison: True')
-        #             equal = True
-        #             return True
-        #         if l1[i] < l2[i]: return True
-        #      else:
-
-        all_l1_int = True
-        for i in range(len(l1)):
-            if type(l1[i]) is list: all_l1_int = False
-        all_l2_int = True
-        for i in range(len(l2)):
-            if type(l2[i]) is list: all_l2_int = False
-
         min_len = min(len(l1), len(l2))
         for i in range(min_len):
-            if equal == False: continue
-            equal = compare(l1[i], l2[i])
-            if all_l1_int and all_l2_int:
-                if i == (min_len-1) and len(l1) > len(l2) and l1[i] == l2[i]:
-                    print(f'Comparison: False')
-                    equal = False
-                    return False
-        
+            res = compare(l1[i], l2[i])
+            if res == 1: return 1
+            if res == -1: return -1
 
+        # check if all were equal and l2 was shorter. Then incorrect order
+        if (l1[min_len-1] == l2[min_len-1]) and len(l1) < len(l2): return -1
 
-    return equal
+        # check if equal but l2 was longer. correct order
+        if (l1[min_len-1] == l2[min_len-1]) and len(l1) > len(l2): return 1
+
+        # if all equal and equal length then ccontinue
+        if (l1[min_len-1] == l2[min_len-1]) and len(l1) == len(l2): return 0
 
 with open("thirt/thirt_input.txt","r") as f:
     lines = f.readlines()
@@ -84,10 +48,7 @@ for i in range(0, len(lines), 2):
     l1 = eval(lines[i].strip())
     l2 = eval(lines[i+1].strip())
     result = compare(l1, l2)
-    print(result)
-    print("")
-    print("")
-    if result: right_order.append(index)
+    if result == -1: right_order.append(index)
 
 print(right_order)
 print(sum(right_order))
