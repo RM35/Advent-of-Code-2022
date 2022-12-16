a75118e7ee4add6a31cb8b28c0a69f6ae312024e
RM35 <92856544+RM35@users.noreply.github.com>
2022-12-16 07:38:52 +0000

    feat: add bfs from point on graph

--- sixt/sixt.py
+++ sixt/sixt.py
@@ -27,35 +27,20 @@ graph = make_graph(data)
 
 # try depth first greeeedy. gets stuck.
 
-q = []
-heapq.heapify(q)
-heapq.heappush(q, graph["AA"])
-
-mins_left = 30
-total_value = 0
-while q and mins_left > 0:
-    node = heapq.heappop(q)
-    max_value_neighbour = 0
-    if len(node[1]) == 0: break
-    if len(node[1]) == 1:
-        heapq.heappush(q, graph[node[1]])
-        mins_left -= 1
-        continue
-    for i, valve in enumerate(node[1]):
-        if i == 0: 
-            max_value_neighbour = valve
-            continue
-        if graph[valve][0] > graph[max_value_neighbour][0]:
-            max_value_neighbour = valve
-    heapq.heappush(q, graph[max_value_neighbour])
-    mins_left -= 1
-    if graph[max_value_neighbour][0] == 0: continue
-    mins_left -= 1
-    total_value += graph[max_value_neighbour][0] * mins_left
-    graph[max_value_neighbour][0] = 0
-    
-
-print(total_value)
+def bfs(graph, start):
+    q = deque()
+    q.append([graph[start], 0])
+    depths = {"AA": 0}
+    while q:
+        node, traversals = q.popleft()
+        for neighbour in node[1]:
+            if neighbour in depths: continue
+            q.append([graph[neighbour], traversals + 1]) 
+            depths[neighbour] = traversals + 1
+    return depths
+        
+a = bfs(graph, "AA")
+pass        
     
 
 
