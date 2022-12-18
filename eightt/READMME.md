63c2f54eafd0086cd2599db97681e7b2db303bea
unknown <92856544+RM35@users.noreply.github.com>
2022-12-18 08:17:54 +0000

    feat: finish outter bfs and re add p1 face counting

--- eightt/READMME.md
+++ eightt/READMME.md
@@ -13,3 +13,9 @@ Finding the air pockets to subtract sounds like a BFS from the outside of the dr
 
 - We then need to represent the 3D space as a graph. Let BFS run within a box bounded by the extremities of the droplet + 1.  Any piece of obsidian it reachs add that to the total faces.
 - Cube is a total of 12K cells so ok
+
+- Try without a grid? simplifiies not having to test boundaries and make sure there is boundaries. Just set the boundaries in the BFS.
+
+- BFS coming out with 30k explored cells after setting the bounds to -2 to 30 for each axis. runs in seconds.
+
+- Now to compare the p1 cells with explored cells and count faces adjacent
---
+++ eightt/eighttp2.py
@@ -0,0 +1,74 @@
+from collections import defaultdict, deque
+from datetime import datetime
+
+droplets = 0
+with open("eightt/eightt_input.txt", "r") as f:
+    data = f.readlines()
+    data = list(map(lambda x: x.strip(), data))
+    for i, line in enumerate(data):
+        data[i] = tuple(map(int, line.split(",")))
+    droplets = set(data)
+
+
+def adjacent(p1, p2):
+    x_delta = abs(p1[0] - p2[0])
+    y_delta = abs(p1[1] - p2[1])
+    z_delta = abs(p1[2] - p2[2])
+    return ((x_delta + y_delta + z_delta) == 1)
+
+face_count = {}
+
+for drop in droplets:
+    face_count[drop] = 6
+
+for p in droplets:
+    for pp in droplets:
+        if adjacent(p, pp):
+            face_count[p] -= 1
+
+q = deque()
+start = (0, 0, 0)
+q.append(start)
+explored = set()
+
+bfs_max_bounds = 30
+bfs_min_bounds = -2
+
+start_time = datetime.now()
+print(f'BFS START: {start_time}')
+
+while q:
+    node = q.popleft()
+    if node in explored: continue
+    explored.add(node)
+
+    # +ve x
+    if node[0] < bfs_max_bounds:
+        if (node[0]+1, node[1], node[2]) not in droplets:
+            q.append((node[0]+1, node[1], node[2]))
+    # -ve x
+    if node[0] > bfs_min_bounds:
+        if (node[0]-1, node[1], node[2]) not in droplets:
+            q.append((node[0]-1, node[1], node[2]))
+    # +ve y
+    if node[1] < bfs_max_bounds:
+        if (node[0], node[1]+1, node[2]) not in droplets:
+            q.append((node[0], node[1]+1, node[2]))
+    # -ve y
+    if node[1] > bfs_min_bounds:
+        if (node[0], node[1]-1, node[2]) not in droplets:
+            q.append((node[0], node[1]-1, node[2]))
+    # +ve z
+    if node[2] < bfs_max_bounds:
+        if (node[0], node[1], node[2]+1) not in droplets:
+            q.append((node[0], node[1], node[2]+1))
+    # -ve z
+    if node[2] > bfs_min_bounds:
+        if (node[0], node[1], node[2]-1) not in droplets:
+            q.append((node[0], node[1], node[2]-1))
+
+print(f'BFS END: {datetime.now()}')
+print(f'BFS TIME: {datetime.now() - start_time}')
+
+pass
+    
